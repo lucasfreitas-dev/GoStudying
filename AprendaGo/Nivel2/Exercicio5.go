f66ca43b28c198eb9e8b1c5d26e10ffeca013336
lucasfreitas-dev <lucas.freitas.dev@gmail.com>
2022-03-24 14:59:33 -0300

    Level 2 ✅

---
+++ AprendaGo/Nivel2/Exercicio5.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := `Essa é uma
+	                raw string 
+	literal`
+
+	fmt.Println(a)
+}
+
+/*
+- Crie uma variável de tipo string utilizando uma raw string literal.
+- Demonstre-a.
+*/
