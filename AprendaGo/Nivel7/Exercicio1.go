19796e6867fac91abac5a16fa9e6ca05ff52bc84
lucasfreitas-dev <lucas.freitas.dev@gmail.com>
2022-04-18 00:56:33 -0300

    Level 7 ✅

---
+++ AprendaGo/Nivel7/Exercicio1.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 10
+
+	fmt.Println(&x)
+}
+
+/*
+- Crie um valor e atribua-o a uma variável.
+- Demonstre o endereço deste valor na memória.
+*/
